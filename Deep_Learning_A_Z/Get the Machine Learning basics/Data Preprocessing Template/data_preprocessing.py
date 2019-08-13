9d4c0f9d538df5e18bf54105bbb7c398a1b9e831
netotz <neto.otz@hotmail.com>
2019-08-13 18:58:42 -0500

    Add dataset splitter

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing.py
@@ -3,8 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 # Importing the dataset
 dataset = pd.read_csv('Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/Data.csv')
@@ -22,5 +24,7 @@ X[:, 0] = lblEncoder.fit_transform(X[:, 0])
 
 oneEncoder = OneHotEncoder(categorical_features = [0])
 X = oneEncoder.fit_transform(X).toarray()
-
 y = lblEncoder.fit_transform(y)
+
+# splitting the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
