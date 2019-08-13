cc8f746db75ba09eab6e91fcd2205b4f9a502bd5
netotz <neto.otz@hotmail.com>
2019-08-13 17:41:54 -0500

    Add handling of missing data

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -1,11 +1,16 @@
 # Data Preprocessing
-
+#%%
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+imputer = SimpleImputer()
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
