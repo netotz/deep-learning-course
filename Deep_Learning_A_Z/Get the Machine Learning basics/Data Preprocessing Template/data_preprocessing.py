c022256e732035afa3a9f9392fc0567851a92c36
netotz <neto.otz@hotmail.com>
2019-08-13 23:20:38 -0500

    Add feature scaling

--- Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing.py
+++ Deep_Learning_A_Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing.py
@@ -31,3 +31,10 @@ y = lblEncoder.fit_transform(y)
 
 # splitting the dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# feature scaling:
+# variables must be in the same range
+scaler = StandardScaler()
+scaler = scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
