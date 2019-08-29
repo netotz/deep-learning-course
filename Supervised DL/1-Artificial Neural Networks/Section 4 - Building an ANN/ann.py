416d616f640298298390e639190ded18884d9be4
netotz <neto.otz@hotmail.com>
2019-08-28 20:28:14 -0500

    Add NN initialization

--- Supervised DL/1-Artificial Neural Networks/Section 4 - Building an ANN/ann.py
+++ Supervised DL/1-Artificial Neural Networks/Section 4 - Building an ANN/ann.py
@@ -1,4 +1,5 @@
 from pandas import read_csv
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 import keras
@@ -29,9 +30,22 @@ X_test = scaler.transform(X_test)
 
 # initialize the neural network
 classifier = Sequential()
-# create hidden layer:
-# 6 nodes = average of nodes in input + nodes in output
-# activation function: rectifier (relu)
-# uniform method to randomly initialize weights
-hidden_layer = Dense(6, activation = 'relu', kernel_initializer = 'uniform')
-classifier.add(hidden_layer)
+
+# add layers to NN
+for i in range(2):
+    # create hidden layer:
+    # 6 nodes = average of nodes in input and nodes in output
+    # activation function: rectifier (relu)
+    # uniform method to randomly initialize weights
+    hidden_layer = Dense(6, activation = 'relu', kernel_initializer = 'uniform')
+    classifier.add(hidden_layer)
+# create output layer
+# 1 node as it is binary output
+# sigmoid because it will predict a probability
+out_layer = Dense(1, activation = 'sigmoid', kernel_initializer = 'uniform')
+classifier.add(out_layer)
+
+# set parameters to create NN
+classifier.compile('adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# fit the model into dataset
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
